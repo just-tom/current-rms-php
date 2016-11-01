4f57bf4974ea7c7262786eae28f3f2d8dd591c6d
Thomas Burman <thomas@lewisecommerce.com>
2016-11-01 19:06:31 +0000

    created base current rms class to handle guzzel client and logging

---
+++ src/CurrentRms.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace CurrentRms;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Uri;
+
+class CurrentRms
+{
+    const POST = 'POST';
+    const GET = 'GET';
+    const PUT = 'PUT';
+    const DELETE = 'DELETE';
+
+    protected $client;
+    protected $config;
+    protected $logger;
+
+    public function __construct()
+    {
+    }
+
+    public function initClient()
+    {
+        $config = $this->getConfigurationValues();
+        $this->client = new Client([
+            'base_uri' => $config['uri'],
+            'headers'  => [
+                'X-AUTH-TOKEN' => $config['token'],
+                'X-SUBDOMAIN'  => $config['subdomain']
+            ],
+            'handler'  => $this->createLoggingHandlerStack('RESPONSE: {code} - {res_body}')
+        ]);
+    }
+
+    public function getConfigurationValues()
+    {
+        return $this->config;
+    }
+
+    public function setConfigurationValues(array $values)
+    {
+        $this->config = $values;
+    }
+
+    public function call(Uri $uri, array $params, $method = CurrentRms::GET)
+    {
+        if (!$this->client) {
+            $this->initClient();
+        }
+
+        $params['http_errors'] = false;
+
+        return $this->client->request($method, $uri, $params);
+    }
+
+    private function getLogger()
+    {
+        if (!$this->logger) {
+            $this->logger = new \Monolog\Logger('api-consumer');
+            $this->logger->pushHandler(
+                new \Monolog\Handler\StreamHandler('logs/api-consumer.log')
+            );
+        }
+
+        return $this->logger;
+    }
+
+    private function createGuzzleLoggingMiddleware($messageFormat)
+    {
+        return \GuzzleHttp\Middleware::log(
+            $this->getLogger(),
+            new \GuzzleHttp\MessageFormatter($messageFormat)
+        );
+    }
+
+    private function createLoggingHandlerStack($messageFormat)
+    {
+        $handlerStack = \GuzzleHttp\HandlerStack::create();
+
+        $handlerStack->push(
+            $this->createGuzzleLoggingMiddleware($messageFormat)
+        );
+
+        return $handlerStack;
+    }
+
+}
